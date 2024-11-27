c9eda93bee425cc772023370ce1d89ea2469aa05
Erik B <basargin.erik@gmail.com>
2024-11-27 20:57:05 +0000

    Add DoublePinchButton documentation
    Co-authored-by: Ryan F <45875515+superturboryan@users.noreply.github.com>

--- Sources/DoublePinch/DoublePinchButton.swift
+++ Sources/DoublePinch/DoublePinchButton.swift
@@ -5,7 +5,32 @@
 
 import SwiftUI
 import WatchKit
-
+/// A wrapper around the standard ``SwiftUI/Button`` that responds to a double-pinch gesture, supporting different gesture modes.
+///
+/// Use `DoublePinchButton` to create a button that can trigger actions in response to a double-pinch gesture,
+/// such as accessibility quick actions or gestures available on newer Apple Watch models. This struct
+/// encapsulates the functionality of a standard SwiftUI `Button` while adding double-pinch gesture support.
+///
+/// - Generic Parameter:
+///   - `Label`: The type of the view that represents the label of the button.
+///
+/// ## Example
+/// ```swift
+/// DoublePinchButton(mode: .accessibilityQuickAction) {
+///     print("Action triggered!")
+/// } label: {
+///     Text("Double Pinch Me")
+/// }
+/// ```
+///
+/// - Parameters:
+///   - mode: The mode(s) defining the types of double-pinch actions to handle. Defaults to `.all`,
+///     which includes all supported modes for the current device.
+///   - action: The action to perform when the button is tapped or the double-pinch gesture is recognized.
+///     This action is executed on the main thread.
+///   - label: A view builder that provides the button's visual content.
+///
+/// - Note: The double-pinch gesture support is added via the `.doublePinch(_:)` modifier.
 struct DoublePinchButton<Label: View>: View {
     
     let mode: DoublePinchMode
